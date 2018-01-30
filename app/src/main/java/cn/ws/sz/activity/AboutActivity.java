83730b47bf9f3b5fcccfdef4d12b86db5f6f9b4e
chen.jianliang <chen.jianliang@whaley.cn>
2018-01-30 17:41:30 +0800

    回退aboutActivity

--- app/src/main/java/cn/ws/sz/activity/AboutActivity.java
+++ app/src/main/java/cn/ws/sz/activity/AboutActivity.java
@@ -1,173 +1,47 @@
 package cn.ws.sz.activity;
 
-import android.annotation.SuppressLint;
-import android.os.Handler;
-import android.os.Message;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.alipay.sdk.app.PayTask;
-import com.android.volley.VolleyError;
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
 import com.baidu.location.Poi;
-import com.google.gson.Gson;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Map;
 
 import cn.ws.sz.R;
-import cn.ws.sz.bean.BusinessStatus;
 import cn.ws.sz.service.LocationService;
-import cn.ws.sz.utils.Constant;
-import cn.ws.sz.utils.PayResult;
-import cn.ws.sz.utils.ToastUtil;
+import cn.ws.sz.utils.Eyes;
 import cn.ws.sz.utils.WSApp;
-import third.volley.VolleyListenerInterface;
-import third.volley.VolleyRequestUtil;
 
 public class AboutActivity extends AppCompatActivity {
 
-    private TextView tvTitle;
-    private LinearLayout llReturnBack;
-    private Button btnPay;
-    private LocationService locationService;
-    private String orderInfo;
-    private static final int SDK_PAY_FLAG = 1;
-    private static final int SDK_AUTH_FLAG = 2;
+	private TextView tvTitle;
+	private LinearLayout llReturnBack;
+	private LocationService locationService;
 
-    @SuppressLint("HandlerLeak")
-    private Handler mHandler = new Handler() {
-        @SuppressWarnings("unused")
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case SDK_PAY_FLAG: {
-                    @SuppressWarnings("unchecked")
-                    PayResult payResult = new PayResult((Map<String, String>) msg.obj);
-                    /**
-                     对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
-                     */
-                    String resultInfo = payResult.getResult();// 同步返回需要验证的信息
-                    String resultStatus = payResult.getResultStatus();
-                    // 判断resultStatus 为9000则代表支付成功
-                    if (TextUtils.equals(resultStatus, "9000")) {
-                        // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
-                        Toast.makeText(AboutActivity.this, "支付成功", Toast.LENGTH_SHORT).show();
-                    } else {
-                        // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
-                        Toast.makeText(AboutActivity.this, "支付失败", Toast.LENGTH_SHORT).show();
-                    }
-                    break;
-                }
-                case SDK_AUTH_FLAG: {
-//                    @SuppressWarnings("unchecked")
-//                    AuthResult authResult = new AuthResult((Map<String, String>) msg.obj, true);
-//                    String resultStatus = authResult.getResultStatus();
-//
-//                    // 判断resultStatus 为“9000”且result_code
-//                    // 为“200”则代表授权成功，具体状态码代表含义可参考授权接口文档
-//                    if (TextUtils.equals(resultStatus, "9000") && TextUtils.equals(authResult.getResultCode(), "200")) {
-//                        // 获取alipay_open_id，调支付时作为参数extern_token 的value
-//                        // 传入，则支付账户为该授权账户
-//                        Toast.makeText(PayDemoActivity.this,
-//                                "授权成功\n" + String.format("authCode:%s", authResult.getAuthCode()), Toast.LENGTH_SHORT)
-//                                .show();
-//                    } else {
-//                        // 其他状态值则为授权失败
-//                        Toast.makeText(PayDemoActivity.this,
-//                                "授权失败" + String.format("authCode:%s", authResult.getAuthCode()), Toast.LENGTH_SHORT).show();
-//
-//                    }
-                    break;
-                }
-                default:
-                    break;
-            }
-        }
-
-        ;
-    };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
-
-        tvTitle = (TextView) findViewById(R.id.title_value);
-        tvTitle.setText("关于万商");
-        llReturnBack = (LinearLayout) findViewById(R.id.returnBack);
-        btnPay = (Button) findViewById(R.id.testPay);
-        llReturnBack.setVisibility(View.VISIBLE);
-
-        btnPay.setOnClickListener(new View.OnClickListener() {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+		Eyes.setStatusBarColor(this, ContextCompat.getColor(this,R.color.title_bg));
 
 
-            @Override
-            public void onClick(View view) {
-                doPay(7);
-            }
-        });
-        llReturnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AboutActivity.this.finish();
-            }
-        });
-    }
+		tvTitle= (TextView) findViewById(R.id.title_value);
+		tvTitle.setText("关于万商");
+		llReturnBack = (LinearLayout) findViewById(R.id.returnBack);
+		llReturnBack.setVisibility(View.VISIBLE);
 
-    public void doPay(int merchantId) {
-        VolleyRequestUtil.RequestGet(this,
-                Constant.PRE_PAY + "/" + merchantId,
-                Constant.TAG_ALIPAY,//商家列表tag
-                new VolleyListenerInterface(this,
-                        VolleyListenerInterface.mListener,
-                        VolleyListenerInterface.mErrorListener) {
-                    @Override
-                    public void onMySuccess(String result) {
-                        JSONObject jsonObject = null;
-                        try {
-                            jsonObject = new JSONObject(result);
-                            int code = jsonObject.getInt("errcode");
-                            if (code == 0) {
-                                orderInfo = jsonObject.getString("data");
-                                Runnable payRunnable = new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        PayTask alipay = new PayTask(AboutActivity.this);
-                                        Map<String, String> result = alipay.payV2(orderInfo, true);
-                                        Log.i("msp", result.toString());
 
-                                        Message msg = new Message();
-                                        msg.what = SDK_PAY_FLAG;
-                                        msg.obj = result;
-                                        mHandler.sendMessage(msg);
-                                    }
-                                };
-                                Thread payThread = new Thread(payRunnable);
-                                payThread.start();
-                            } else {
-                                ToastUtil.showShort(AboutActivity.this, jsonObject.getString("data"));
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    @Override
-                    public void onMyError(VolleyError error) {
-
-                    }
-                },
-                true);
-    }
+		llReturnBack.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				AboutActivity.this.finish();
+			}
+		});
+	}
 
 }
