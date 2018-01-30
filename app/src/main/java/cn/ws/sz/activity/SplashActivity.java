3157e4352cb59cacd83e2e4a82072409ccb65cf9
BruceChen5683 <BruceChen_foreign@126.com>
2018-01-31 00:53:45 +0800

    权限处理

--- app/src/main/java/cn/ws/sz/activity/SplashActivity.java
+++ app/src/main/java/cn/ws/sz/activity/SplashActivity.java
@@ -1,10 +1,16 @@
 package cn.ws.sz.activity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.Window;
 
@@ -17,9 +23,11 @@ import cn.ws.sz.bean.AreaStatus;
 import cn.ws.sz.bean.CityStatus;
 import cn.ws.sz.bean.ClassifyStatus;
 import cn.ws.sz.bean.ProvinceStatus;
+import cn.ws.sz.utils.Authority;
 import cn.ws.sz.utils.Constant;
 import cn.ws.sz.utils.DataHelper;
 import cn.ws.sz.utils.Eyes;
+import cn.ws.sz.utils.PermissionsChecker;
 import cn.ws.sz.utils.ToastUtil;
 import cn.ws.sz.utils.WSApp;
 import third.ACache;
@@ -39,15 +47,121 @@ public class SplashActivity extends Activity {
     private ACache mCache;
 
 
+    private boolean isRequireCheck; // 是否需要系统权限检测
+    private PermissionsChecker mPermissionsChecker;//检查权限
+    //危险权限（运行时权限）
+    static final String[] PERMISSIONS = new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+    private static final int PERMISSION_REQUEST_CODE = 0;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
         Eyes.setStatusBarColor(this, ContextCompat.getColor(this,R.color.title_bg));
+
+        mPermissionsChecker = new PermissionsChecker(this);
+        isRequireCheck = true;
         gson = new Gson();
         mCache = ACache.get(this);
-        loadData();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /**
+         * 6.0权限
+         */
+        if (isRequireCheck) {
+            //权限没有授权，进入授权界面
+            if(mPermissionsChecker.judgePermissions(PERMISSIONS)){
+                ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST_CODE);
+            }else{
+                loadData();
+            }
+        }else{
+            isRequireCheck = true;
+        }
+
+//
+    }
+
+    /**
+     * 用户权限处理,
+     * 如果全部获取, 则直接过.
+     * 如果权限缺失, 则提示Dialog.
+     * @param requestCode  请求码
+     * @param permissions  权限
+     * @param grantResults 结果
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String[] permissions,  int[] grantResults) {
+
+
+        Log.e(TAG, "onRequestPermissionsResult: requestCode  "+requestCode );
+
+        for (int i = 0;i < permissions.length;i++){
+            Log.e(TAG, "onRequestPermissionsResult: "+permissions[i]);
+        }
+
+        if (requestCode == PERMISSION_REQUEST_CODE && hasAllPermissionsGranted(grantResults)) {
+            isRequireCheck = true;
+            loadData();
+        } else {
+            isRequireCheck = false;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                showPermissionDialog();
+            }
+        }
+    }
+
+    // 含有全部的权限
+    private boolean hasAllPermissionsGranted( int[] grantResults) {
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                Log.e(TAG, "hasAllPermissionsGranted: "+grantResult+"");
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * 提示对话框
+     */
+    private void showPermissionDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this);
+        builder.setTitle("帮助");
+        builder.setMessage("当前应用缺少必要权限。请点击\"设置\"-打开所需权限。");
+        // 拒绝, 退出应用
+        builder.setNegativeButton("退出", new DialogInterface.OnClickListener() {
+            @Override public void onClick(DialogInterface dialog, int which) {
+//                setResult(PERMISSIONS_DENIED);
+                finish();
+            }
+        });
+
+        builder.setPositiveButton("设置", new DialogInterface.OnClickListener() {
+            @Override public void onClick(DialogInterface dialog, int which) {
+                startAppSettings();
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
+    }
+    // 启动应用的设置
+    private void startAppSettings() {
+        Intent appDetailSettingIntent = Authority.getAppDetailSettingIntent(this);
+        startActivity(appDetailSettingIntent);
+
     }
 
     private void loadData() {
@@ -272,7 +386,9 @@ public class SplashActivity extends Activity {
         for (int i = 0;i < DataHelper.getInstance().getFirstCategroyList().size();i++){
             loadingSecondData(DataHelper.getInstance().getFirstCategroyList().get(i).getId());
         }
-        startMainActivity();
+        if(isRequireCheck){
+            startMainActivity();
+        }
     }
 
     private void parseSecondCategroy(String result, int id,boolean save) {
