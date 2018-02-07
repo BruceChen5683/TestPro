cf0008a8c2e28d1842c00ee7084d0a5bc20c86fe
BruceChen5683 <BruceChen_foreign@126.com>
2018-02-07 21:58:14 +0800

    支付成功后

--- app/src/main/java/cn/ws/sz/activity/MoneyActivity.java
+++ app/src/main/java/cn/ws/sz/activity/MoneyActivity.java
@@ -128,23 +128,7 @@ public class MoneyActivity extends AppCompatActivity implements View.OnClickList
     private StringBuilder imageIdStr = new StringBuilder();
 
     private static final int UPLOAD_PIC_SUCCESS = 200;
-    private Handler mHandler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what){
-                case UPLOAD_PIC_SUCCESS:
-                    Log.d(TAG, "handleMessage: "+mImageSize);
-                    if(mImageSize == SelectedImages.size()){
-                        addMerchant();
-                    }else {
 
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
 
     /*
      *  choose
@@ -185,59 +169,46 @@ public class MoneyActivity extends AppCompatActivity implements View.OnClickList
 	 * */
 	private String orderInfo;
 	private static final int SDK_PAY_FLAG = 1;
-	@SuppressLint("HandlerLeak")
-	private Handler mPayHandler = new Handler() {
-		@SuppressWarnings("unused")
-		public void handleMessage(Message msg) {
-			switch (msg.what) {
-				case SDK_PAY_FLAG: {
-					@SuppressWarnings("unchecked")
-					PayResult payResult = new PayResult((Map<String, String>) msg.obj);
-					/**
-					 对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
-					 */
-					String resultInfo = payResult.getResult();// 同步返回需要验证的信息
-					String resultStatus = payResult.getResultStatus();
-					// 判断resultStatus 为9000则代表支付成功
-					if (TextUtils.equals(resultStatus, "9000")) {
-						// 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
-						Toast.makeText(MoneyActivity.this, "支付成功", Toast.LENGTH_SHORT).show();
-					} else {
-						// 该笔订单真实的支付结果，需要依赖服务端的异步通知。
-						Toast.makeText(MoneyActivity.this, "支付失败", Toast.LENGTH_SHORT).show();
-					}
-					break;
-				}
-				default:
-					break;
-			}
-		}
-		;
-	};
 
 
 
-
-    private Handler handler = new Handler(){
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
-
             switch (msg.what){
-                case MSG_PAY_SUCCESS:
-                    Intent intent = new Intent();
-                    intent.setClass(MoneyActivity.this,AfterPayActivity.class);
-                    startActivity(intent);
+                case UPLOAD_PIC_SUCCESS:
+                    Log.d(TAG, "handleMessage: "+mImageSize);
+                    if(mImageSize == SelectedImages.size()){
+                        addMerchant();
+                    }else {
+
+                    }
                     break;
-                case MSG_PAY_FAIL:
+                case SDK_PAY_FLAG:
+                    PayResult payResult = new PayResult((Map<String, String>) msg.obj);
+                    /**
+                     对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
+                     */
+                    String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+                    String resultStatus = payResult.getResultStatus();
+                    // 判断resultStatus 为9000则代表支付成功
+                    if (TextUtils.equals(resultStatus, "9000")) {
+                        // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
+                        Toast.makeText(MoneyActivity.this, "支付成功", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent();
+                        intent.setClass(MoneyActivity.this,AfterPayActivity.class);
+                        startActivity(intent);
+                    } else {
+                        // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
+                        Toast.makeText(MoneyActivity.this, "支付失败", Toast.LENGTH_SHORT).show();
+                    }
                     break;
                 default:
                     break;
             }
         }
     };
-
-    public MoneyActivity() {
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
